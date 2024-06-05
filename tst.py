3d490ff6983ecef8d90a9615ff0ec71b69a79d45
valeria wabwire <larriebliss@gmail.com>
2024-06-05 15:01:09 +0300

    game

---
+++ tst.py
@@ -0,0 +1,15 @@
+# x=50
+# y=100
+# print("Before x=",x,"y=",y)
+# x,y=y,x
+# print("After x=",x,"y=",y)
+# x, y, z = "Orange", "Banana", "Cherry"
+# print(x)
+# print(y)
+# print(z)
+# name = input("what is your name ?  ")
+# print("hello", name)
+# color = input("what is your favorite color  ")
+# print(name,"likes", color)
+print(10%3)
+
