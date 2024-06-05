0b4378ebd68422cc55a081f9a1a5946c7487ab5a
valeria wabwire <larriebliss@gmail.com>
2024-06-05 15:21:12 +0300

    finished game

--- tst.py
+++ tst.py
@@ -1,15 +1,18 @@
-# x=50
-# y=100
-# print("Before x=",x,"y=",y)
-# x,y=y,x
-# print("After x=",x,"y=",y)
-# x, y, z = "Orange", "Banana", "Cherry"
-# print(x)
-# print(y)
-# print(z)
-# name = input("what is your name ?  ")
-# print("hello", name)
-# color = input("what is your favorite color  ")
-# print(name,"likes", color)
-print(10%3)
+print("This is a guessing game")
+print("Guess a name of a wild animal that is tall, you have three attempts.Good luck!")
+animal_name = "girrafe"
+guess =""
+guess_count = 0
+guess_limit = 3
+out_of_guesses = False
+while guess != animal_name and not (out_of_guesses):
+    if guess_count < guess_limit:
+       guess = input("Enter your guess: ")
+       guess_count += 1
+    else:
+        out_of_guesses = True
 
+if out_of_guesses:    
+    print("out of guesses,YOU LOSE!")
+else:
+    print("You win!")
